9df127502159a84500932a996adaa387f43eaff2
andresdeinnocentiis <andresdeinnocentiis@gmail.com>
2022-10-10 23:53:04 -0300

    entrega Curso1

--- backend_spotBA/base/urls.py
+++ backend_spotBA/base/urls.py
@@ -3,4 +3,7 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('products/', views.products, name='products'),
+    path('about/', views.about, name='about'),
+    path('contact/', views.contact, name='contact'),
 ]
--- backend_spotBA/base/views.py
+++ backend_spotBA/base/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.db.models import Q
+from base.models import Product, Category
 
 # Create your views here.
 
@@ -7,8 +9,33 @@ from django.http import HttpResponse
 #    return HttpResponse('Testing OK.')
 
 
+def base(request):
+    params = {}
+    params['site_name'] = 'SpotBA'
+    return render(request, 'base.html', params)
 
 def index(request):
     params = {}
-    params['nombre_sitio'] = 'SpotBA'
+    params['site_name'] = 'SpotBA'
+    categories = Category.objects.all()
+    params['categories'] = categories
     return render(request, 'base/index.html', params)
+
+def products(request):
+    params = {}
+    params['site_name'] = 'SpotBA'
+    products = Product.objects.filter(Q(currency="USD"))
+    params['products'] = products
+    return render(request, 'pages/products.html', params)
+
+def about(request):
+    params = {}
+    params['site_name'] = 'SpotBA'
+    
+    return render(request, 'pages/about.html', {})
+
+def contact(request):
+    params = {}
+    params['site_name'] = 'SpotBA'
+    
+    return render(request, 'pages/contact.html', params)
